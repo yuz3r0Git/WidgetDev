1d8d5d5ecdcb22bdb2ebd4aff9114a897683f8ef
HayatoInoue-git <57336594+HayatoInoue-git@users.noreply.github.com>
2024-11-19 14:31:04 +0000

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by HayatoInoue on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
